78a22bd679b03c0dfa02ba938e8c42d9251d5698
ganesh613-USM <ganesh6133.usm@gmail.com>
2026-02-12 12:22:48 +0530

    Delete tests directory

--- tests/Admin/Model/SectionModelConfigurationTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class SectionModelConfigurationTest extends TestCase
-{
-    public function test()
-    {
-        $this->markTestSkipped('Todo add test for SectionModelConfigurationTest');
-    }
-}
